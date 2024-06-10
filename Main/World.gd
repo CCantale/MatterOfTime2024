c7b8391e063ab0ad8428c7b429a55de0ee89b9b3
Claudio Cantale <claudio.cantale93@gmail.com>
2024-06-10 05:22:54 +0200

    Flipper and Door ready and shift from objects to tiles
    Collectibles are not objects any more, but tiles.
    This makes the interface look a lot cleaner.

--- Main/World.gd
+++ Main/World.gd
@@ -2,23 +2,27 @@ extends Node2D
 
 const dir = preload("res://Global/enumDirections.gd")
 
-var obstacles = []
+var obstacles: Array[Vector2i] = []
 var timeCanMove = true
 var timeLastposition : Vector2
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var obstaclesLayer : int
 	
 	for layerID in $TileMap.get_layers_count():
 		if ($TileMap.get_layer_name(layerID) == "Obstacles"):
 			obstaclesLayer = layerID
-	obstacles = $TileMap.get_used_cells(obstaclesLayer)
+	self.obstacles = $TileMap.get_used_cells(obstaclesLayer)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func reset():
+	$Time.reset()
+	$TileMap.reset()
+
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+
+
 
 func _on_time_is_moving(timePosition, tileLength, direction):
 	var destination : Vector2
@@ -40,3 +44,6 @@ func _on_time_is_moving(timePosition, tileLength, direction):
 	else:
 		timeLastposition = timePosition
 		timeCanMove = true
+
+func _on_time_died():
+	reset()
