077baecfcb61891928b0c19961bed26d130e90d4
Claudio Cantale <claudio.cantale93@gmail.com>
2024-10-08 00:52:06 +0200

    Level 6 and camera animation Afor levels 5 and 6. Window stretch mode changed to Vewport.
    Still need to enable jumpers.

--- Scripts/cameraMovements.gd
+++ Scripts/cameraMovements.gd
@@ -4,7 +4,10 @@ const cameraMovements = {
 	Vector2(1216, 320): "Level 2-1",
 	Vector2(1856, 192): "Level 2-3",
 	Vector2(1984, 192): "Level 3-2",
-	Vector2(2880, 1088): "Level 3-4"
+	Vector2(2880, 1088): "Level 3-4",
+	Vector2(1856, 960): "Level 4-5",
+	Vector2(1600, 1216): "Level 5-6",
+	Vector2(1728, 1216): "Level 6-5"
 }
 	
 const povCamera = {
